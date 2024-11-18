5c6766d56242e6d3e922df1b6c6d95b7933a8fe5
Pritam Chatterjee <74919879+Pritam2023@users.noreply.github.com>
2024-11-18 22:11:56 +0530

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -10,8 +10,8 @@ const Home = () => {
 
       <div className="body">
         <span className="title">
-          Software Component <br />
-          Cataloguing Software
+         Welcome to <br />
+          Software Component Cataloguing Software
         </span>
         <p className="desc">
         The Software Component Cataloging Software addresses the challenge of efficiently managing and retrieving reusable software components. Built with features such as user authentication, hierarchical categorization, and real-time database updates using ReactJS and Firebase, it streamlines the cataloging process, enabling users to store, search, and reuse components with ease in diverse software development environments.
