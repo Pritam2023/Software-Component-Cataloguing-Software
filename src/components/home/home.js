9cbb88d0d0db8a193957802b1bdde30842dea1b0
Pritam Chatterjee <74919879+Pritam2023@users.noreply.github.com>
2024-11-19 01:35:55 +0530

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../header/header";
 import "./home.css";
 import { Link } from "react-router-dom";
-
+import reactLogo from "./Image.png"
 const Home = () => {
   return (
     <>
